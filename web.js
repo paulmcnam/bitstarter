058e2691a7d4a6a0781c7540f2c1f8dee6066f82
Paul Mc Namara <paul@paulmcnamara.com>
2013-07-10 17:58:51 +0000

    with checking

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+    var fs = require('fs');
     var buffer = new Buffer(16);
     buffer.write(fs.readFileSync('index.html'));
-    response.send(buffer.toString();
+    response.send(buffer.toString());
 //response.send('Hello World 2!');
 });
 
