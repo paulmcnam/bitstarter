eb9b51c4037f88ff8e1c78ebd47099f40d671fe4
Paul Mc Namara <paul@paulmcnamara.com>
2013-07-10 17:45:49 +0000

    now with reading file

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = new Buffer(16);
+    buffer.write(fs.readFileSync('index.html'));
+    response.send(buffer.toString();
+//response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
